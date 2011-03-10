fc73a9382943b7bfdae497367c610aa8692f8cdf
Ali Anwar <ali@alis-lappy>
2011-03-11 02:40:52 +0500

    City: A start

--- db/schema.rb
+++ db/schema.rb
@@ -9,11 +9,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110310165401) do
+ActiveRecord::Schema.define(:version => 20110310213949) do
 
   create_table "categories", :force => true do |t|
     t.string "name"
     t.string "parent"
+  end
+
+  create_table "cities", :force => true do |t|
+    t.string "name"
+    t.string "region"
   end
 
   create_table "users", :force => true do |t|
